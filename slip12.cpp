4f2fb02789a90ea7402e835252f148c4c2f642e3
sapate vaibhav <64630274+sapatevaibhav@users.noreply.github.com>
2023-05-30 13:35:15 +0530

    added animation code slip12

--- slip12.cpp
+++ slip12.cpp
@@ -1,68 +1,43 @@
 // Implement animation principles for any object
 
-#include <iostream>
 #include <GL/glut.h>
-#include <GL/glu.h>
 
-float triangleX = 0.0;
+int x = 0;
+int direction = 1;
 
 void display()
 {
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glColor3f(1, 0, 0);
     glClear(GL_COLOR_BUFFER_BIT);
-
-    glLoadIdentity();
-    glTranslatef(triangleX, 0.0, 0.0);
-
-    glBegin(GL_TRIANGLES);
-    glVertex3f(0.0, 0.0, 0.0);
-    glVertex3f(1.0, 0.0, 0.0);
-    glVertex3f(0.5, 1.0, 0.0);
+    glColor3f(1, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2i(x, 220);
+    glVertex2i(x + 40, 220);
+    glVertex2i(x + 40, 260);
+    glVertex2i(x, 260);
     glEnd();
-
     glutSwapBuffers();
 }
 
-void update(int value)
+void timer(int)
 {
-    triangleX += 0.01;
     glutPostRedisplay();
-    glutTimerFunc(16, update, 0);
-}
-
-void reshape(int width, int height)
-{
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0, (float)width / (float)height, 1.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-void keyboard(unsigned char key, int x, int y)
-{
-    switch (key)
+    glutTimerFunc(1000 / 60, timer, 0);
+    x += direction * 3;
+    if (x == 600 || x == 0)
     {
-    case 27:
-        exit(0);
-        break;
+        direction *= -1;
     }
 }
 
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(500, 500);
-    glutCreateWindow("Moving Triangle");
-
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("Animation");
+    glOrtho(0, 640, 0, 480, -1, 1);
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
-    glutTimerFunc(0, update, 0);
-
+    glutTimerFunc(0, timer, 0);
     glutMainLoop();
-
     return 0;
 }
